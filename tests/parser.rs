d53f1ca7f767dc5fcf1c11692c20ae6dab6d01b3
Kaonan Micadei <k.micadei@gmail.com>
2021-11-04 15:31:51 +0100

    rename tests

--- tests/parser.rs
+++ tests/parser.rs
@@ -4,18 +4,18 @@ use parser::*;
 use parser::Sexpr::*;
 
 #[test]
-fn nil() {
+fn parse_nil() {
     assert_eq!(parse_line("()"), Nil);
 }
 
 #[test]
-fn bool() {
+fn parse_bool() {
     assert_eq!(parse_line("true"), Bool(true));
     assert_eq!(parse_line("false"), Bool(false));
 }
 
 #[test]
-fn number() {
+fn parse_number() {
     assert_eq!(parse_line("1"), Number(1.0));
     assert_eq!(parse_line("2.1"), Number(2.1));
     assert_eq!(parse_line("314e-2"), Number(3.14));
@@ -23,19 +23,19 @@ fn number() {
 }
 
 #[test]
-fn text() {
+fn parse_text() {
     assert_eq!(parse_line("\"this is a  test\""), Text("this is a  test".to_string()));
 }
 
 #[test]
-fn symbol() {
+fn parse_symbol() {
     assert_eq!(parse_line("pi"), Symbol("pi".to_string()));
     assert_eq!(parse_line("my-var"), Symbol("my-var".to_string()));
     assert_eq!(parse_line("+1"), Symbol("+1".to_string()));
 }
 
 #[test]
-fn list() {
+fn parse_list() {
     let val = List(vec![
         Symbol("+".to_string()), 
         Symbol("x".to_string()), 
